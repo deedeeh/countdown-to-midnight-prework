177af8ee905d02dd52ebc1000b7ea0d4a190af51
Dina Hafez <dinahafez.8@gmail.com>
2018-06-23 12:47:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 
-# def countdown(n)
-#   counter = n
-#   while counter >= 1
-#     puts "#{counter} SECOND(S)!"
-#     counter -= 1
-#   end
-#   return "HAPPY NEW YEAR!"
-# end
+def countdown(n)
+  counter = n
+  while counter >= 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
 def countdown_with_sleep(n)
   counter = n
