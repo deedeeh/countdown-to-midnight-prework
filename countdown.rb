98b4fab55c4f9d5ed2f4e4cc62f5fb583298d223
Dina Hafez <dinahafez.8@gmail.com>
2018-06-23 12:45:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,11 @@ def countdown(n)
 end
 
 def countdown_with_sleep(n)
-  sleep n
+  counter = n
+  while counter >= 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep 5
+  end
+  return "HAPPY NEW YEAR!"
 end
